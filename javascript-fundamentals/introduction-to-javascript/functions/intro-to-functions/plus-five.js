82c5bd30aced7c65962d116479e52a52663b2d9a
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-06 02:07:20 +0300

    Add plusFive function with its documentation

---
+++ javascript-fundamentals/introduction-to-javascript/functions/intro-to-functions/plus-five.js
@@ -0,0 +1,17 @@
+/**
+ * This function takes in a number as an argument
+ * and returns the sum of that number and 5.
+ *
+ * @param {number} num
+ *     This is the number to add 5 to.
+ * 
+ * @returns
+ *     the result of adding 5 to the given number 
+ */
+function plusFive (num) {
+  return num + 5;
+}
+
+console.log(plusFive(10)); // => 15
+console.log(plusFive(2)); // => 7
+console.log(plusFive(-8)); // => -3
