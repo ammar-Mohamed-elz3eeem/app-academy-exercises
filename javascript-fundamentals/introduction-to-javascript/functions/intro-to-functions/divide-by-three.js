0e865ed7c2a8f9d59f3663b2f22b7c11542d0d1c
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-06 02:05:55 +0300

    Add divideByThree function with its documentation

---
+++ javascript-fundamentals/introduction-to-javascript/functions/intro-to-functions/divide-by-three.js
@@ -0,0 +1,17 @@
+/**
+ * This function returns the passed in number 
+ * argument divided by three.
+ *
+ * @param {number} num
+ *     This is the number to divide by 3.
+ *
+ * @returns
+ *     return the given string divided by 3.
+ */
+function divideByThree(num) {
+  return num / 3;
+}
+
+console.log(divideByThree(9)); // => 3
+console.log(divideByThree(15)); // => 5
+console.log(divideByThree(98)); // => 32.666666666666664
