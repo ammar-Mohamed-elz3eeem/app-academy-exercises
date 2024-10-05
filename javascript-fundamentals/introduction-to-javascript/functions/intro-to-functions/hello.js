73d3511d7fa5c3a2f066ebd67f0a4e1628d3c9c5
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-06 02:06:22 +0300

    Add hello function with its documentation

---
+++ javascript-fundamentals/introduction-to-javascript/functions/intro-to-functions/hello.js
@@ -0,0 +1,14 @@
+/**
+ * This function takes in a string and prints out
+ * "Hello, " followed by the string.
+ *
+ * @param {string} str
+ *     This is the string to prepend with "Hello, ".
+ */
+function hello (str) {
+	console.log(`Hello, ${str}`);
+}
+
+hello("cat"); // prints "Hello, cat"
+hello("Helen"); // prints "Hello, Helen"
+hello("Sting"); // prints "Hello, Sting"
