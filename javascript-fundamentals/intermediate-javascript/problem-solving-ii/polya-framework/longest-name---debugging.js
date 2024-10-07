bac9042cfe837ad23393c54ea30bc3b28e5179cb
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-07 07:27:56 +0300

    Solve Problem Solving II exercises

---
+++ javascript-fundamentals/intermediate-javascript/problem-solving-ii/polya-framework/longest-name---debugging.js
@@ -0,0 +1,32 @@
+/**
+ * This function takes an array of names and returns
+ * the longest. If there's a tie, it returns the first
+ * name of the longest length.
+ *
+ * @param {Array} names
+ *     This is the array containing list of names to
+ *     operate on and extract longest name from.
+ *
+ * @returns
+ *     returns the longest. If there's a tie, it returns
+ *     the first name of the longest length.
+ */
+function longestName (names) {
+  // Set the first name to be the longest
+  let currentLongest = names[0];
+  // Check each other name in the array starting from the second
+  for (let i = 1; i < names.length; i++) {
+    // If the name we're checking is longer than our
+    // current longest, set that name to be the new longest
+    if (names[i].length > currentLongest.length) {
+      currentLongest = names[i];
+    }
+  }
+  return currentLongest;
+}
+
+testNames = ["James", "Patricia", "Michael", "Elizabeth", "Christopher",
+             "Sarah", "Margaret", "Kenneth", "Stephanie", "Jonathan",
+             "Jeremy", "Samantha", "Alexander", "Catherine", "Benjamin"]
+
+console.log(longestName(testNames)); // "Christopher"
