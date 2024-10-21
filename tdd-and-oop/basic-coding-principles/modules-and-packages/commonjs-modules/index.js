69f8caa90bd4b9d0c1d8d5601598c5b26d056b98
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-21 18:10:33 +0300

    Finish common js modules project

---
+++ tdd-and-oop/basic-coding-principles/modules-and-packages/commonjs-modules/index.js
@@ -0,0 +1,13 @@
+
+// Your code here
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const sayHelloTo = require('./send-messages/say-hello-to');
+const { msg1, msg2, msg3 } = require('./messages');
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+sayHelloTo("Mr. Potato");
+giveMessageToMrsPotato(msg1);
+giveMessageToMrsPotato(msg2);
+giveMessageToMrsPotato(msg3);
